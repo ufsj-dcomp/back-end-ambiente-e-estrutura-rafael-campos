5ef00504088a96424774b7cae543cd4708bc01df
rafael henrique campos <rhcsjdr@outlook.com>
2020-10-15 21:04:55 -0300

    atualização

--- myapp/app.js
+++ myapp/app.js
@@ -8,7 +8,6 @@ app.get('/aplicativo', (req, res) => {
 })
 app.get('/html',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 app.post('/imagens', function (req, res) {
