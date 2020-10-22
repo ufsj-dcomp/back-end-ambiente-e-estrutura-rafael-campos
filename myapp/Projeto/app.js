6235ef98ba94563abb306f6e57b52682eaf74886
rafael henrique campos <rhcsjdr@outlook.com>
2020-10-22 18:58:11 -0300

    Adicionando mais uma aba
    + aba + debug

--- myapp/Projeto/app.js
+++ myapp/Projeto/app.js
@@ -3,24 +3,45 @@ var app     = express();
 
 app.use(express.json());
 
-app.post('/player', (req, res) => {
+app.post('/jogador', (req, res) => {
     var player = req.body;
     console.log(player);    
 })
 
-app.get('/player/:playerId', (req, res) => {
+app.get('/jogador/:playerId', (req, res) => {
     var playerId = req.params.playerId;
     console.log("GET PlayerId: " + playerId);    
 })
 
-app.put('/player/:playerId', (req, res) => {
+app.put('/jogador/:playerId', (req, res) => {
     var playerId = req.params.playerId;
     console.log("PUT PlayerId: " + playerId);    
 })
 
-app.delete('/player/:playerId', (req, res) => {
+app.delete('/jogador/:playerId', (req, res) => {
     var playerId = req.params.playerId;
     console.log("DELETE PlayerId: " + playerId);    
+})
+
+
+app.post('/user', (req, res) => {
+    var user = req.body;
+    console.log(user);    
+})
+
+app.get('/user/:userId', (req, res) => {
+    var playerId = req.params.userId;
+    console.log("GET UserId: " + playerId);    
+})
+
+app.put('/user/:userId', (req, res) => {
+    var playerId = req.params.userId;
+    console.log("PUT UserId: " + userId);    
+})
+
+app.delete('/user/:userId', (req, res) => {
+    var playerId = req.params.playerId;
+    console.log("DELETE UserId: " + userId);    
 })
 
 app.listen(3000, () => {
