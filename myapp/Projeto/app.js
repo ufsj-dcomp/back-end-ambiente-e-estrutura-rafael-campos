57407665249d3ef41f137569a8fcac2d7c7d4509
rafael henrique campos <rhcsjdr@outlook.com>
2020-10-22 22:30:08 -0300

    debug app js

--- myapp/Projeto/app.js
+++ myapp/Projeto/app.js
@@ -3,43 +3,43 @@ var app     = express();
 
 app.use(express.json());
 
-app.post('/jogador', (req, res) => {
+app.post('/jogador', (req, resp) => {
     var player = req.body;
     console.log(player);    
 })
 
-app.get('/jogador/:playerId', (req, res) => {
+app.get('/jogador/:playerId', (req, resp) => {
     var playerId = req.params.playerId;
-    console.log("GET PlayerId: " + playerId);    
+    console.log("GET - PlayerId: " + playerId);    
 })
 
-app.put('/jogador/:playerId', (req, res) => {
+app.put('/jogador/:playerId', (req, resp) => {
     var playerId = req.params.playerId;
-    console.log("PUT PlayerId: " + playerId);    
+    console.log("PUT - PlayerId: " + playerId);    
 })
 
-app.delete('/jogador/:playerId', (req, res) => {
+app.delete('/jogador/:playerId', (req, resp) => {
     var playerId = req.params.playerId;
-    console.log("DELETE PlayerId: " + playerId);    
+    console.log("DELETE - PlayerId: " + playerId);    
 })
 
 
-app.post('/user', (req, res) => {
+app.post('/user', (req, resp) => {
     var user = req.body;
     console.log(user);    
 })
 
-app.get('/user/:userId', (req, res) => {
+app.get('/user/:userId', (req, resp) => {
     var playerId = req.params.userId;
     console.log("GET UserId: " + playerId);    
 })
 
-app.put('/user/:userId', (req, res) => {
+app.put('/user/:userId', (req, resp) => {
     var playerId = req.params.userId;
     console.log("PUT UserId: " + userId);    
 })
 
-app.delete('/user/:userId', (req, res) => {
+app.delete('/user/:userId', (req, resp) => {
     var playerId = req.params.playerId;
     console.log("DELETE UserId: " + userId);    
 })
