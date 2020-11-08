39d70c1c6ea0a56fcdc5aa2209c456c30dfcee6d
rafael henrique campos <rhcsjdr@outlook.com>
2020-11-08 10:32:52 -0300

    atualização app.js

--- myapp/Projeto/node-mysql/app-online/app.js
+++ myapp/Projeto/node-mysql/app-online/app.js
@@ -1,53 +1,65 @@
-var mysql = require('mysql');
-var connection = require('express-myconnection');
-var createError = require('http-errors');
+require('getmodule');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var connection = require('express-myconnection');
+var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-app.use(
-  connection(mysql,{
-    host: 'localhost',
-    user: 'rafael',
-    password : 'rh181908',
-    port : 3306,
-    database:'debanco_dados'
-  },'request')
-);
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    connection(mysql,{
+        host: 'localhost',
+        user: 'root',
+        password : 'rh181908',
+        port : 3306,
+        database:'banco_dados'
+    },'request')
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/users', users);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
--- myapp/Projeto/node-mysql/app-online/routes/index.js
+++ myapp/Projeto/node-mysql/app-online/routes/index.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var express = require('express');
-var router = express.Router();
+var App = express.Router();
+var jogadores = getmodule('api/jogadores');
 
 /* GET home page. */
-router.get('/jogadores', function(req, res) {
-  req.getConnection(function(err,connection){
-        connection.query('SELECT * FROM tb_jogadores', [], function(err, result){
-if (err) {
-                    return res.status(400).json(err);
-             }
-            return res.status(200).json(result);
-        });
-  });
-});
-module.exports = router;
+App.route('/jogadores')
+	.get(jogadores.list)
+	.post(jogadores.create);
+
+App.route('/jogadores/:id')
+	.get(jogadores.getById)
+	.put(jogadores.update)
+	.delete(jogadores.delete);
+
+module.exports = App;
