6e4ef0f8eb6aaaa43f88a25c72c322031a0a70be
rafael henrique campos <rhcsjdr@outlook.com>
2020-11-08 12:57:12 -0300

    agora vai

---
+++ myapp/Projeto/bau-node-mysql/bau-online/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/jogadores', function(req, res) {
+  req.getConnection(function(err,connection){
+        connection.query('SELECT * FROM tb_jogadores', [], function(err, result){
+if (err) {
+                    return res.status(400).json(err);
+             }
+            return res.status(200).json(result);
+        });
+  });
+});
+module.exports = router;
