c497ced567102023fd4a90cd0c15bff28f683e7d
rafael henrique campos <rhcsjdr@outlook.com>
2020-11-12 09:07:33 -0300

    atualização porta bd

--- myapp/Projeto2/crud/server.js
+++ myapp/Projeto2/crud/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 // Setup server port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
