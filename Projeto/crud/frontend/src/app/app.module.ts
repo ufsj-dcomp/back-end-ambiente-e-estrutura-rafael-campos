35aad601cfbd11a4f0696c1cdc61d3d9b92106c4
rafael henrique campos <rhcsjdr@outlook.com>
2020-11-19 10:38:55 -0300

    atualização front end

--- Projeto/crud/frontend/src/app/app.module.ts
+++ Projeto/crud/frontend/src/app/app.module.ts
@@ -7,9 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { JogadorComponent } from './jogador/jogador.component';
 import { HttpClientModule} from '@angular/common/http';
 import { JogadorService } from  './jogador.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,10 @@ import { JogadorService } from  './jogador.service';
     MatToolbarModule,
     MatButtonModule,
     MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule
   ],
--- Projeto/crud/frontend/src/app/jogador/jogador.component.ts
+++ Projeto/crud/frontend/src/app/jogador/jogador.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { JogadorService } from '../jogador.service';
 
-export class Jogador{
+export class Jogador {
   id: number;
   nome: string;
   sobrenome: string;
@@ -11,7 +13,6 @@ export class Jogador{
   status: string;
 }
 
-
 @Component({
   selector: 'app-jogador',
   templateUrl: './jogador.component.html',
@@ -20,12 +21,30 @@ export class Jogador{
 export class JogadorComponent implements OnInit {
 
   displayedColumns: string[] = ['id','nome','sobrenome','time','posicao','idade','status'];
-  dataSource = JOGADORES;
+  dataSource = Jogador;
 
-  constructor(private service: JogadorService) { }
+  constructor(private service: JogadorService, public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.service.getJogador().subscribe(jogadores => this.dataSource = jogadores);
+    this.service.getJogador().subscribe(jogadores => this.dataSource = Jogador);
   }
 
 }
+
+@Component({
+  selector: 'dialog-mng-jogador',
+  templateUrl: 'dialog-mng-jogador.html'
+})
+
+export class MngJogadorDialog {
+  
+  constructor(public dialogRef: MatDialogRef<MngJogadorDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: Jogador) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }  
+  
+}
+
+
